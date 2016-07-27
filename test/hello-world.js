50703c8d1fa110889f8a229405e69b83f0294f16
edawg11 <eliot.smith1185@gmail.com>
2016-07-27 09:33:28 -0400

    first commit

---
+++ test/hello-world.js
@@ -0,0 +1,13 @@
+import assert from 'assert';
+import helloWorld from '../src/scripts/hello-world';
+
+describe('helloWorld', () => {
+
+	describe('#init()', () => {
+
+		it('should return hello world', () =>{
+
+			assert.equal(helloWorld.init(), 'hello world');
+		});
+	});
+});
